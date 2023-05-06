2034f9cf5b1859479197eb6dd4820d79adc500c8
Ivan Mieres <mieres.iv@gmail.com>
2023-05-06 20:44:17 -0300

    colocando nombre a las entities y configurando el application.properties

--- src/main/java/com/sublimado/eCommerce/model/DesingType.java
+++ src/main/java/com/sublimado/eCommerce/model/DesingType.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -19,4 +16,5 @@ public class DesingType {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long idType;
     private String type;
+    private Desing desing;
 }
