798bec351985078d1581974078fa4208d502a541
Ivan Mieres <mieres.iv@gmail.com>
2023-05-08 00:29:08 -0300

    creando mapeo de entities y capa persistencia jpa

--- src/main/java/com/sublimado/eCommerce/model/Product.java
+++ src/main/java/com/sublimado/eCommerce/model/Product.java
@@ -1,17 +1,15 @@
 package com.sublimado.eCommerce.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class Product {
 
